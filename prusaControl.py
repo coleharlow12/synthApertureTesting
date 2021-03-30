c284e76ead0776e7fabbc49a88d60acf6bb258dc
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2021-03-30 14:27:51 -0400

    File with basic script to control the 3D printer using python

---
+++ prusaControl.py
@@ -0,0 +1,33 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/tty.usbmodem14201', 115200)
+
+time.sleep(2)
+ser.write(b'G28\r\n')
+
+readSer=True
+while readSer:
+	a=ser.readline().decode('UTF-8')
+	if a=="ok\n":
+		readSer=False
+
+ser.write(b'G1 X200.000 Y10.000 E0 F12000\r\n')
+
+readSer=True
+while readSer:
+	a=ser.readline().decode('UTF-8')
+	print(a)
+	if a=="ok\n":
+		readSer=False
+
+ser.write(b'G1 X0 Y10.000 E0 F12000\r\n')
+
+readSer=True
+while readSer:
+	a=ser.readline().decode('UTF-8')
+	print(a)
+	if a=="ok\n":
+		readSer=False
+		
+ser.close()
