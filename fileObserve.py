afa880d927ad6fdf5056590d2bdbcb2304c40885
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2021-03-31 11:39:03 -0400

    Now points to binary file generated from the mmWaveStudio. It detects changes in the written file. Note that there are two updates to st_mtime. Once when the binary file is cleared and once when it is re-written

--- fileObserve.py
+++ fileObserve.py
@@ -2,18 +2,22 @@ import os
 
 class Monkey(object):
 	def __init__(self):
-		self.cached_stamp=0
-		self.filename= r'C:\Users\ColeHarlow\Documents\GitHub\synthApertureTesting\fileObserve.py'
+		self.filename= r'C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\adc_data_Raw_0.bin'
+		self.cached_stamp= os.stat(self.filename).st_mtime
 
 	def ook(self):
 		stamp = os.stat(self.filename).st_mtime
 		if stamp != self.cached_stamp:
-			self._cached_stamp = stamp
+			self.cached_stamp = stamp
 			print(stamp)
 
 
 fChng = Monkey()
 
 fChng.ook()
+print(fChng.cached_stamp)
+
+while(True):
+	fChng.ook()
 
 #fdffda
