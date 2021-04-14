78ea3d0c0315093847019b8af034228fc478c046
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2021-04-14 15:50:42 -0400

    40 was a slow motion of the vibratons, 41 was me in the chair

--- fullControl.py
+++ fullControl.py
@@ -18,14 +18,14 @@ setup(ser,maxX,maxZ)
 Meas = Measurement()
 
 # Specify the Measurement Locations
-mLocs = PreMeas(xStart=5,zStart=80,xSteps=24,zSteps=4,xSpace=2.5,zSpace=2.5,path=Meas.storPathPy)
+mLocs = PreMeas(xStart=5,zStart=80,xSteps=8,zSteps=8,xSpace=2.5,zSpace=2.5,path=Meas.storPathPy)
 mLocs.genPoints()
 mLocs.savePoints()
 
 for coordIn in range(0,(mLocs.measLocs.shape[0])):
 	# Move to the First Measurement Location
 	DoMove(ser=ser,coords=mLocs.measLocs[coordIn,:],xSpeed=maxX,zSpeed=maxZ)
-	time.sleep(3)
+	time.sleep(0)
 	Meas.takeMeasure()
 
 
