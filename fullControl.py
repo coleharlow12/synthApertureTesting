e151ea640384293d9816eb5a1def931aebc69e9b
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2021-04-20 14:34:35 -0400

    Measurement. All 3 TX used. 20 Chirps per tx. 200 Samples per chirp. 5 mm spacing between aperture measurements

--- fullControl.py
+++ fullControl.py
@@ -18,14 +18,14 @@ setup(ser,maxX,maxZ)
 Meas = Measurement()
 
 # Specify the Measurement Locations
-mLocs = PreMeas(xStart=5,zStart=80,xSteps=8,zSteps=8,xSpace=2.5,zSpace=2.5,path=Meas.storPathPy)
+mLocs = PreMeas(xStart=5,zStart=80,xSteps=8,zSteps=8,xSpace=5,zSpace=5,path=Meas.storPathPy)
 mLocs.genPoints()
 mLocs.savePoints()
 
 for coordIn in range(0,(mLocs.measLocs.shape[0])):
 	# Move to the First Measurement Location
 	DoMove(ser=ser,coords=mLocs.measLocs[coordIn,:],xSpeed=maxX,zSpeed=maxZ)
-	time.sleep(0)
+	time.sleep(1)
 	Meas.takeMeasure()
 
 
