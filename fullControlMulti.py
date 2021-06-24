c4807c127f71a2a4fb7021a2cc7fc22f46657281
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2021-06-24 11:34:48 -0400

    Allows for multiple arrays to be created on the 3d printer aperture

---
+++ fullControlMulti.py
@@ -0,0 +1,37 @@
+import os
+import time
+import serial
+import pdb
+import numpy as np
+
+from HelperFuncs.printerHelp import setup,DoMove
+from HelperFuncs.measControl import Measurement
+from HelperFuncs.preMeasure import PreMeas
+
+# Connect the Port For the 3D printer
+ser = serial.Serial(port = "COM3", baudrate=115200)
+maxX = 25 #mm/s
+maxZ = 25 #mm/s
+
+start=np.array([[15, 30],[150,30]])
+print(start[0][0])
+
+#pdb.set_trace()
+
+for i in np.arange(0,start.shape[0]):
+	#Setup the printer
+	setup(ser,maxX,maxZ)
+
+	# Create the Measurement Object
+	Meas = Measurement()
+
+	# Specify the Measurement Locations
+	mLocs = PreMeas(xStart=start[i][0],zStart=start[i][1],xSteps=8,zSteps=16,xSpace=5,zSpace=2.5,path=Meas.storPathPy)
+	mLocs.genPoints()
+	mLocs.savePoints()
+
+	for coordIn in range(0,(mLocs.measLocs.shape[0])):
+		# Move to the First Measurement Location
+		DoMove(ser=ser,coords=mLocs.measLocs[coordIn,:],xSpeed=maxX,zSpeed=maxZ)
+		time.sleep(0)
+		Meas.takeMeasure()
