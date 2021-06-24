0f42ddf1eee90b505db17f294acf794b776c2730
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2021-06-24 13:45:10 -0400

    NOw its working

--- fullControlMulti.py
+++ fullControlMulti.py
@@ -13,25 +13,37 @@ ser = serial.Serial(port = "COM3", baudrate=115200)
 maxX = 25 #mm/s
 maxZ = 25 #mm/s
 
-start=np.array([[15, 30],[150,30]])
+start=np.array([[5, 30],[170,30]])
 print(start[0][0])
 
 #pdb.set_trace()
 
-for i in np.arange(0,start.shape[0]):
-	#Setup the printer
-	setup(ser,maxX,maxZ)
+#Setup the printer
+setup(ser,maxX,maxZ)
 
-	# Create the Measurement Object
-	Meas = Measurement()
+# Create the Measurement Object
+Meas = Measurement()
 
-	# Specify the Measurement Locations
-	mLocs = PreMeas(xStart=start[i][0],zStart=start[i][1],xSteps=8,zSteps=16,xSpace=5,zSpace=2.5,path=Meas.storPathPy)
-	mLocs.genPoints()
-	mLocs.savePoints()
+# Specify the Measurement Locations
+mLocs = PreMeas(xStart=start[0][0],zStart=start[0][1],xSteps=8,zSteps=16,xSpace=5,zSpace=2.5,path=Meas.storPathPy)
+mLocs.genPoints()
+mLocs.savePoints()
 
-	for coordIn in range(0,(mLocs.measLocs.shape[0])):
-		# Move to the First Measurement Location
-		DoMove(ser=ser,coords=mLocs.measLocs[coordIn,:],xSpeed=maxX,zSpeed=maxZ)
-		time.sleep(0)
-		Meas.takeMeasure()
+for coordIn in range(0,(mLocs.measLocs.shape[0])):
+	# Move to the First Measurement Location
+	DoMove(ser=ser,coords=mLocs.measLocs[coordIn,:],xSpeed=maxX,zSpeed=maxZ)
+	time.sleep(0)
+	#Meas.takeMeasure()
+
+Meas1 = Measurement(initMat=False)
+
+# Specify the Measurement Locations
+mLocs = PreMeas(xStart=start[1][0],zStart=start[1][1],xSteps=8,zSteps=16,xSpace=5,zSpace=2.5,path=Meas1.storPathPy)
+mLocs.genPoints()
+mLocs.savePoints()
+
+for coordIn in range(0,(mLocs.measLocs.shape[0])):
+	# Move to the First Measurement Location
+	DoMove(ser=ser,coords=mLocs.measLocs[coordIn,:],xSpeed=maxX,zSpeed=maxZ)
+	time.sleep(0)
+	#Meas1.takeMeasure()
